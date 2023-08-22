e169c73fbdf42825e6c98d98ba70cfea6cb59e08
Przemek <krepski80@gmail.com>
2023-08-22 21:03:27 +0200

    first templates

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,24 +2,35 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private array $message = ['hello', 'hi', 'morning'];
+
     #[Route(path: '/{limit<\d+>?3}', name: 'app_index')]
     public function index(int $limit): Response
     {
-
-        return new Response(implode(',',array_slice($this->message,0,$limit))
+        return $this->render('hello/index.html.twig',
+            [
+                'message' => implode(',', array_slice($this->message, 0, $limit))
+            ]
         );
+
     }
 
 
-#[Route('/message/{id<\d+>}', name: 'show-one')]
+    #[Route('/message/{id<\d+>}', name: 'show-one')]
     public function showOne(int $id): Response
     {
-        return new Response($this->message[$id]);
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->message[$id]
+            ]
+        );
+
     }
 }
